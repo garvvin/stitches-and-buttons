95cbaaca234d320bcf9737882e01a1a1781660af
garvvin <yugarvinyu@gmail.com>
2025-04-23 23:27:07 -0700

    added title animation and yarn

--- src/app/components/Navbar/Navbar.js
+++ src/app/components/Navbar/Navbar.js
@@ -1,35 +1,86 @@
-'use client'
-import NavbarBtn from './NavbarBtn'
+'use client';
+import NavbarBtn from './NavbarBtn';
 
-import { useState } from 'react'
-import Link from 'next/link'
-import Image from 'next/image'
+import { useState } from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+
+//css
+import '../../css/title.css';
+import '../../css/yarnline.css';
+
+//components
+import Title from './Title';
+import Yarnline from './Yarnline';
 
 export default function Navbar() {
-    const [open, setOpen] = useState(false) 
+	const [open, setOpen] = useState(false);
 
-    return (
-        <nav className='w-full flex flex-col h-20 absolute top-0 left-0'>
-            <div className={`flex flex-col duration-200 relative overflow-hidden items-center ${open ? 'min-h-screen' : 'min-h-full'} bg-white-500 z-40`}>
-                <div className={`flex flex-row min-h-20 place-content-center items-center h-20 gap-8 lg:gap-5 mr-5`}>
-                    <span className={'inline mx-3 cursor-pointer absolute left-5 top-6 duration-500 size-15 hover:size-17 ' + (open ? '-rotate-280' : 'rotate-none')} onClick={() => setOpen(!open)}>
-                        <Image src='/images/nav-button.png' alt='navigation button' width={100} height={100} />
-                    </span>
-                    <Link href="/"><span className='font-semibold text-pink-300 text-3xl' style={{fontFamily:'caveat'}}>Stiches & Buttons</span></Link>
-                </div>
+	return (
+		<nav className="w-full flex flex-col h-20 absolute top-0 left-0">
+			<div
+				className={`flex flex-col duration-200 relative overflow-hidden items-center ${open ? 'min-h-screen' : 'min-h-full'} bg-white-500 z-40`}
+			>
+				<div
+					className={`flex flex-row min-h-20 place-content-center items-center h-20 gap-8 lg:gap-5 mr-5`}
+				>
+					<span
+						className={
+							'inline mx-3 cursor-pointer absolute left-0 top-4 lg:left-5 lg:top-6 duration-500 size-15 hover:size-17 ' +
+							(open ? '-rotate-280' : 'rotate-none')
+						}
+						onClick={() => setOpen(!open)}
+					>
+						<Image
+							src="/images/nav-button.png"
+							alt="navigation button"
+							width={100}
+							height={100}
+							priority={true}
+						/>
+					</span>
+					<Link href="/">
+						<span className="relative">
+							<Yarnline />
+							<Title />
+							<Image
+								src="/images/yarn.png"
+								alt="Pink yarn ball"
+								width={100}
+								height={100}
+								className="absolute top-0 left-11 lg:-translate-x-30 -translate-x-12 w-18 h-13 duration-200 lg:-translate-x-34 lg:translate-y-5 lg:w-25 lg:h-18"
+							/>
+						</span>
+					</Link>
+				</div>
 
-                <ul className={`flex flex-col w-50 text-center absolute left-5 top-20`}>
-                    <li>
-                        <NavbarBtn href='/about-us' extraStyle={'rotate-3 hover:-rotate-3 active:-rotate-5'}>My Shop</NavbarBtn>
-                    </li>
-                    <li>
-                        <NavbarBtn href='/locations' extraStyle={'-rotate-3 hover:rotate-3 active:rotate-5'}>About Me</NavbarBtn>
-                    </li>
-                    <li>
-                        <NavbarBtn href='/contact' extraStyle={'rotate-3 hover:-rotate-3 active:-rotate-5'}>Contact</NavbarBtn>
-                    </li>
-                </ul>
-            </div>
-        </nav>
-    )
+				<ul className={`flex flex-col w-50 text-center absolute left-5 top-20`}>
+					<li>
+						<NavbarBtn
+							href="/about-us"
+							extraStyle={'rotate-3 hover:-rotate-3 active:-rotate-5'}
+						>
+							My Shop
+						</NavbarBtn>
+					</li>
+					<li>
+						<NavbarBtn
+							href="/locations"
+							extraStyle={'-rotate-3 hover:rotate-3 active:rotate-5'}
+						>
+							About Me
+						</NavbarBtn>
+					</li>
+					<li>
+						<NavbarBtn
+							href="/contact"
+							extraStyle={'rotate-3 hover:-rotate-3 active:-rotate-5'}
+						>
+							Contact
+						</NavbarBtn>
+					</li>
+				</ul>
+			</div>
+		</nav>
+	);
 }
--- src/app/components/Navbar/NavbarBtn.js
+++ src/app/components/Navbar/NavbarBtn.js
@@ -1,11 +1,16 @@
-import Link from 'next/link'
+import Link from 'next/link';
 
 export default function NavbarBtn({ href, extraStyle, children }) {
-    return (
-        <Link href={href}>
-            <span className={"duration-300 block text-center rounded-xl my-4 px-6 min-h-15 flex items-center hover:bg-zinc-100 bg-zinc-50 border-3 border-pink-300 text-pink-300 hover:text-pink-800 " + extraStyle}>
-                {children}
-            </span>
-        </Link>
-    )
+	return (
+		<Link href={href}>
+			<span
+				className={
+					'duration-300 block rounded-xl my-4 px-6 min-h-15 flex justify-center items-center hover:bg-zinc-100 bg-zinc-50 border-3 border-pink-300 text-pink-300 hover:text-pink-800 ' +
+					extraStyle
+				}
+			>
+				{children}
+			</span>
+		</Link>
+	);
 }
